7fb216ab035fded0bf06d99fb8906b548fad343d
rania3104 <raniaazizfarooqi.14574.11g@gmail.com>
2022-12-30 14:48:48 +0400

    base code (needs a lottttt of changes)

--- Code.py
+++ Code.py
@@ -1,9 +1,7 @@
-#opening message that introduces the restaurant
-print("Welcome to Burgermania!\nHere is what we have to ofFer:")
-items_in_stock = [
+items = [
     {
         "item_id": 0,
-        "item_name": "Milky Bar",
+        "item_name": "C",
         'item_price': 60,
     },
     {
@@ -28,47 +26,30 @@ items_in_stock = [
     },
 ]
 
+quit = False
+item = ''
 
-the_item = []
+while quit == False:
+    print("Welcome to Burgermania!\nHere is what we have to ofFer:")
+    for i in items:
+        print(f"Item Name: {i['item_name']} - Price: {i['item_price']} - code: {i['item_id']}")
 
-reciept = """
-\t\tPRODUCT -- PRICE
-"""
-
-sum = 0
-
-run = True
-
-print("------- Vending Machine with Python-------\n\n")
-print("----------The Items In Stock Are----------\n\n")
-
-for i in items_in_stock:
-    print(f"Item: {i['item_name']} --- Price: {i['item_price']} --- Item ID: {i['item_id']}")
-
-
-def machine(items_in_stock, run, the_item):
-    while run:
-
-        buy_item = int(input("\n\nEnter the item code of the product you want to buy: "))
-
-        if buy_item < len(items_in_stock):
-            the_item.append(items_in_stock[buy_item])
-        else:
-            print("THE PRODUCT ID IS WRONG!")
-
-        more_items = str(input("press any key to add more items and press q to quit.. "))
-
-        if more_items == "q":
-            run = False
-
-    rec_bool = int(input(("1. print the reciept? 2. only print the total sum .. ")))
-    if rec_bool == 1:
-        print(create_reciept(the_item, reciept))
-    elif rec_bool == 2:
-        print(sum(the_item))
+    order = int(input("Enter the code number of the item you want to get: "))
+    for i in items:
+        if order == i['item_id']:
+            item = i
+    if item == '':
+        print('This code is invalid.')
     else:
-        print("INVALID ENTRY")
+        print(f"Great, {item['item_name']} will cost you {item['item_price']} dollars")
 
+        price = int(input(f"Enter {item['item_price']} dollars to purchase: "))
+        if price == item['item_price']:
+            print(f"Thank you for buying here is your {item['item_name']}")
+        else:
+            print(f"Please enter only {item['item_price']} dollars")
+
+    order = input("To quit the machine enter q and to continue buying enter anything: ")
 
 def sum(the_item):
     sum = 0
@@ -90,8 +71,10 @@ def create_reciept(the_item, reciept):
         
         
         """
-    return reciept
+    return r
 
-
-if __name__ == "__main__":
-    machine(items_in_stock, run, the_item)
+if order == 'c':
+    quit = False
+else:
+    quit = True
+print('')
