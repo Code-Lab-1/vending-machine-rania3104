f0150f936e07df709c4d83639ffc079015525d51
rania3104 <raniaazizfarooqi.14574.11g@gmail.com>
2023-01-02 17:18:07 +0400

    base

--- Code.py
+++ Code.py
@@ -1,76 +1,64 @@
 #Rania Aziz Farooqi
 #Storing the menu indicating different categories of food.
 Burgers=[{
-    "id": 101, #ID Number
+    "id": 1, #ID Number
     "name": "Chicken Burger", 
     'price': 10,
-    "stock": 29
-    }, #The amount that is is in stock
+    },
 {
-    "id": 102,
+    "id": 2,
     "name": "Beef Burger",
     'price': 13,
-    "stock": 32,
     },
 {
-    "id": 103,
+    "id": 3,
     "name": "Zinger Burger",
     'price': 18,
-    "stock": 13
     },
 {
-    "id": 104,
+    "id": 4,
     "name": "Cheeseburger",
     'price': 9,
-    "stock": 4,
     },]
 Drinks=[{
-    "id": 105,
+    "id": 5,
     "name": "Original Hot Coffee",
     'price': 10,
-    "stock": 48,
     },
 {
-    "id": 106,
+    "id": 6,
     "name": "Iced Latte",
     'price': 20,
-    "stock": 22,
     },
 {
-    "id": 107,
+    "id": 7,
     "name": "Pepsi",
     'price': 4,
-    "stock": 80,
     },
 {
-    "id": 108,
+    "id": 8,
     "name": "Water",
     'price': 2,
-    "stock": 0,
     },]
 Add_Ons=[{
-    "id": 109,
+    "id": 9,
     "name": "Extra Fries",
     'price': 10,
-    "stock": 8,
     },
 {
-    "id": 110,
+    "id": 10,
     "name": "Cheeseballs",
     'price': 8,
-    "stock": 12,
     },
 {
-    "id": 111,
+    "id": 11,
     "name": "Chicken Nuggets",
     'price': 14,
-    "stock": 19,
     },
 {
-    "id": 112,
+    "id": 12,
     "name": "Cookie",
     'price': 5,
-    "stock": 57,
     }
 ]
 
@@ -88,40 +76,38 @@ reciept = """
 
 sum = 0
 
-def machine(Menu,quit,the_item):
-    while quit == False:
-        print("Welcome to Burgermania!\nHere is what we have to ofFer:")
-        
-        print("1 - Burgers")
-        for i in Burgers:
-            print(f"Item ID: {i['id']} - Item Name: {i['name']} - Price: {i['price']}")
-        print("2 - Drinks")
-        for i in Drinks:
-            print(f"Item ID: {i['id']} - Item Name: {i['name']} - Price: {i['price']}")
-        print("3 - Add Ons")
-        for i in Add_Ons:
-            print(f"Item ID: {i['id']} - Item Name: {i['name']} - Price: {i['price']}")
-        
-        order = int(input("Enter the ID of the item you want to order: "))
+print("Welcome to Burgermania!\nHere is what we have to ofFer:")
+print("1 - Burgers")
+for i in Burgers:
+    print(f"Item ID: {i['id']} - Item Name: {i['name']} - Price: {i['price']}")
+print("2 - Drinks")
+for i in Drinks:
+    print(f"Item ID: {i['id']} - Item Name: {i['name']} - Price: {i['price']}")
+print("3 - Add Ons")
+for i in Add_Ons:
+    print(f"Item ID: {i['id']} - Item Name: {i['name']} - Price: {i['price']}")
 
-        def your_order(category):
-            if order in category:
-                the_item.append(category.values(order))
-                category['stock'] -= 1
-                if category['stock']==0:
-                    print("Sorry the item you requested for is currently out of stock.")
-            else:
-                print('This code is invalid.')
-        question = input("To quit the machine enter q and to continue buying enter anything: ")
+def machine(quit,the_item):
+    while quit == False:
+
+        order = int(input("Enter the ID of the item you want to order: "))
+        if order <=4 and order>=1:
+            the_item.append(Burgers[order])
+        elif order <=8:
+            the_item.append(Drinks[order])
+        elif order <=12:
+            the_item.append(Add_Ons[order])         
+        else:
+            print('This code is invalid.')
         
-        your_order(Menu)
+        print("You order till now:",the_item)        
 
         more_items = str(input("press any key to add more items and press q to quit.. "))
         if more_items == "q":
             quit=True
         
-        print(sum(the_item))
-        print(create_reciept(the_item, reciept))
+    print("Here is the total: ",sum(the_item))
+    print(create_reciept(the_item, reciept))
 
 def sum(the_item):
     sum = 0
@@ -146,4 +132,4 @@ def create_reciept(the_item, reciept):
     return reciept
 
 if __name__ == "__main__":
-    machine(Menu,quit,the_item) 
+    machine(quit,the_item) 
