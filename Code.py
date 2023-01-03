d22f459cf724409783e050c8bbd0a6b5e019b10d
rania3104 <raniaazizfarooqi.14574.11g@gmail.com>
2023-01-03 12:48:57 +0400

    base

--- Code.py
+++ Code.py
@@ -1,62 +1,62 @@
 #Rania Aziz Farooqi
 #Storing the menu indicating different categories of food.
 Burgers=[{
-    "id": 1, #ID Number
+    "id": 0, #ID Number
     "name": "Chicken Burger", 
     'price': 10,
     },
 {
-    "id": 2,
+    "id": 1,
     "name": "Beef Burger",
     'price': 13,
     },
 {
-    "id": 3,
+    "id":2,
     "name": "Zinger Burger",
     'price': 18,
     },
 {
-    "id": 4,
+    "id": 3,
     "name": "Cheeseburger",
     'price': 9,
     },]
 Drinks=[{
-    "id": 5,
+    "id": 4,
     "name": "Original Hot Coffee",
     'price': 10,
     },
 {
-    "id": 6,
+    "id": 5,
     "name": "Iced Latte",
     'price': 20,
     },
 {
-    "id": 7,
+    "id": 6,
     "name": "Pepsi",
     'price': 4,
     },
 {
-    "id": 8,
+    "id": 7,
     "name": "Water",
     'price': 2,
     },]
 Add_Ons=[{
-    "id": 9,
+    "id": 8,
     "name": "Extra Fries",
     'price': 10,
     },
 {
-    "id": 10,
+    "id": 9,
     "name": "Cheeseballs",
     'price': 8,
     },
 {
-    "id": 11,
+    "id": 10,
     "name": "Chicken Nuggets",
     'price': 14,
     },
 {
-    "id": 12,
+    "id": 11,
     "name": "Cookie",
     'price': 5,
     }
@@ -91,16 +91,18 @@ def machine(quit,the_item):
     while quit == False:
 
         order = int(input("Enter the ID of the item you want to order: "))
-        if order <=4 and order>=1:
+        if (order <3 or order ==3):
             the_item.append(Burgers[order])
-        elif order <=8 or order >=5:
-            the_item.append(Drinks[order])
-        elif order <=12 or order >=9:
-            the_item.append(Add_Ons[order])         
+        elif (order <7 or order ==7):
+            the_item.append(Drinks[order-4])
+        elif order <11 or order ==11:
+            the_item.append(Add_Ons[order-8])         
         else:
             print('This code is invalid.')
         
-        print("Your order till now:",the_item)        
+        print("Your order till now: ")  
+        for i in the_item:
+            print(f"Item ID: {i['id']} - Item Name: {i['name']} - Price: {i['price']}")    
 
         more_items = str(input("Enter to add more items or press q to quit.. "))
         if more_items == "q":
